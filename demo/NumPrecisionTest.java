560a6d863ee8eee313f90957fc5dbc2526fa2af6
“liulei” <871607680@qq.com>
2022-08-18 09:30:26 +0800

    3 precision

---
+++ demo/NumPrecisionTest.java
@@ -0,0 +1,15 @@
+import aaa;
+
+/**
+ * 进制转换造成的小数精度问题 10.000002
+ */
+public class NumPrecisionTest {
+
+    public static void main(String[] args) {
+        float num = 0;
+        for(int i = 1;i<=100;i++) {
+            num += 0.1;
+        }
+        System.out.println(num);
+    }
+}
